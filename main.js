9bcd8040057cb77326a2642016fd6b9353abc08a
Andrea Pérez <andreaa.perz@gmail.com>
2021-03-12 19:41:44 -0600

    Practica 3 Fetch

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let win
 function createWindow(){
 	win = new BrowserWindow ({width: 800, height:600})
 	win.loadURL(url.format({
-		pathname: path.join(__dirname, './fetch/practica2.html'),
+		pathname: path.join(__dirname, './fetch/practica3.html'),
 		protocol: 'file',
 		slashes: true
 	}))
