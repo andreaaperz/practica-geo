1547a4e92dff96faad7d626fdf44a3e25bd36a03
Andrea Pérez <andreaa.perz@gmail.com>
2021-03-25 10:51:41 -0600

    practica 9 y 10

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let win
 function createWindow(){
 	win = new BrowserWindow ({width: 800, height:600})
 	win.loadURL(url.format({
-		pathname: path.join(__dirname, 'practica8.html'),
+		pathname: path.join(__dirname, 'menu.html'),
 		protocol: 'file',
 		slashes: true
 	}))
