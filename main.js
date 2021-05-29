f3a0d73e9d73681ebb939362c354738a2e9cebd8
Andrea Pérez <andreaa.perz@gmail.com>
2021-05-28 20:19:17 -0500

    Places

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let win
 function createWindow(){
 	win = new BrowserWindow ({width: 800, height:600})
 	win.loadURL(url.format({
-		pathname: path.join(__dirname, 'firebase/amigos/index.html'),
+		pathname: path.join(__dirname, 'practica13.html'),
 		protocol: 'file',
 		slashes: true
 	}))
